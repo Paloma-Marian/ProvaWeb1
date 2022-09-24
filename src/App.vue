58fb5424f584e0591adfcaefb3526bbe4e845da4
PalomaMarian <paloma.marian.designer@gmail.com>
2022-09-23 21:00:29 -0300

    calculoPronto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Calculo de Média de Aproveitamento</h1>
+
+    <label>Nome do Aluno:</label> <br>
+    <input type="text" v-model="nome" required> <br>
+    <label>1ª Nota:</label> <br>
+    <input type="number" v-model="nota1" required> <br>
+    <label>2ª Nota:</label> <br>
+    <input type="number" v-model="nota2" required> <br>
+    <label>3ª Nota:</label> <br>
+    <input type="number" v-model="nota3" required> <br>
+    <p>nota: {{nota1}} nota: {{nota2}} nota: {{nota3}} nome: {{nome}}</p>
+
+    <media-prova
+      :nomeAluno="nome"
+      :valor1="nota1"
+      :valor2="nota2"
+      :valor3="nota3">
+    </media-prova>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MediaProva from './components/MediaProva.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MediaProva
+  },
+  data(){
+    return{
+      nota1: 0,
+      nota2: 0,
+      nota3: 0,
+      nome: ' '
+    }
   }
 }
 </script>
---
+++ src/components/MediaProva.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <button @click="calcularMedia()">Calcular</button>
+    <p>{{media}}</p>
+    <table>
+        <tr>
+            <th>Nome do Aluno</th>
+            <th>Informação</th>
+        </tr>
+        <tr v-for="t in tabelaMedia" :key="t.aluno" >
+            <td>{{t.aluno}}</td>
+            <td>{{t.infomacao}}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        nomeAluno: String,
+        valor1: Number,
+        valor2: Number,
+        valor3: Number
+    },
+    data() {
+        return {
+            aprovado: ' ',
+            media: 0,
+            tabelaMedia:[
+                {
+                    aluno: this.nomeAluno,
+                    infomacao: ''
+                }
+            ]
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        calcularMedia() {
+            this.media = (this.valor1 + this.valor2 * 2 + this.valor3 * 3)/6
+            if(this.nomeAluno != ' '){
+                if (this.media >= 7)  {
+                    this.aprovado = 'Aprovado'  
+                }else{
+                    this.aprovado = 'Reprovado'
+                }
+                this.tabelaMedia.push({
+                aluno: this.nomeAluno,
+                infomacao: this.aprovado
+                })
+            }
+        }
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
